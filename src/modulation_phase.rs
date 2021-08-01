bd1bef32975e9c267f76f0c0d12fdc6b36f382f8
René Hansen <rene@hansen-audio.de>
2021-08-01 16:37:13 +0200

    Learn how to use Rust

--- src/modulation_phase.rs
+++ src/modulation_phase.rs
@@ -1,4 +1,4 @@
-#[repr(C)]
+#[repr(C)] // give this struct/union/enum the same layout and ABI C would
 #[derive(Copy, Clone)]
 pub enum SyncMode {
     FreeRunning,
@@ -6,8 +6,9 @@ pub enum SyncMode {
     ProjectSync,
 }
 
+#[repr(C)]
 #[derive(Copy, Clone)]
-pub struct Phase {
+pub struct Context {
     tempo: f32,
     rate: f32,
     sample_rate_recip: f32,
@@ -16,7 +17,7 @@ pub struct Phase {
     free_running_factor: f32,
     tempo_synced_factor: f32,
     note_len: f32,
-} //
+}
 
 static RECIPROCAL_BEATS_IN_NOTE: f32 = 1. / 4.;
 static RECIPROCAL_60_SECONDS: f32 = 1. / 60.;
@@ -55,71 +56,74 @@ fn compute_tempo_synced_factor(sixty_seconds_recip: f32, tempo: f32) -> f32 {
     return sixty_seconds_recip * tempo;
 }
 
-#[no_mangle]
-pub extern "C" fn create() -> Phase {
-    let phase = Phase {
-        tempo: 120.,
-        rate: 0.1,
-        sample_rate_recip: 1. / 48000.,
-        project_time: 0.,
-        mode: SyncMode::ProjectSync,
-        free_running_factor: 0.,
-        tempo_synced_factor: 0.,
-        note_len: 1. / 32.,
-    };
-
-    phase
-}
-
-#[no_mangle]
-pub extern "C" fn advance(phase: Phase, value: &mut f32, num_samples: i32) -> bool {
-    match phase.mode {
-        SyncMode::FreeRunning => update_free_running(value, num_samples, phase.free_running_factor),
-        SyncMode::TempoSync => update_tempo_sync(value, num_samples, phase.tempo_synced_factor),
-        SyncMode::ProjectSync => {
-            let old_phase = *value;
-            *value = update_project_sync(phase.project_time, phase.rate);
-            return *value < old_phase;
-        }
-    };
-
-    return check_overflow(value, PHASE_MAX);
-}
-
-#[no_mangle]
-pub fn note_length_to_rate(value: f32) -> f32 {
+fn note_length_to_rate(value: f32) -> f32 {
     assert!(value > 0.);
     return (1. / value) * RECIPROCAL_BEATS_IN_NOTE;
 }
 
-#[no_mangle]
-pub extern "C" fn set_rate(cx: &mut Phase, value: f32) {
-    cx.rate = value;
-}
+impl Context {
+    #[no_mangle]
+    pub extern "C" fn set_project_time(&mut self, value: f32) {
+        (*self).project_time = value;
+    }
 
-#[no_mangle]
-pub extern "C" fn set_note_len(cx: &mut Phase, value: f32) {
-    cx.note_len = value;
-    let rate = note_length_to_rate(value);
-    set_rate(cx, rate);
-}
+    #[no_mangle]
+    pub extern "C" fn create() -> Context {
+        let phase = Context {
+            tempo: 120.,
+            rate: 0.1,
+            sample_rate_recip: 1. / 48000.,
+            project_time: 0.,
+            mode: SyncMode::ProjectSync,
+            free_running_factor: 0.,
+            tempo_synced_factor: 0.,
+            note_len: 1. / 32.,
+        };
 
-#[no_mangle]
-pub extern "C" fn set_project_time(cx: &mut Phase, value: f32) {
-    cx.project_time = value;
-}
+        return phase;
+    }
 
-#[no_mangle]
-pub extern "C" fn set_sync_mode(cx: &mut Phase, value: SyncMode) {
-    cx.mode = value;
-}
+    #[no_mangle]
+    pub extern "C" fn set_sync_mode(&mut self, value: SyncMode) {
+        self.mode = value;
+    }
 
-#[no_mangle]
-pub extern "C" fn set_sample_rate(cx: &mut Phase, value: f32) {
-    cx.sample_rate_recip = 1. / value;
-    cx.free_running_factor = compute_free_running_factor(cx.rate, cx.sample_rate_recip);
-    cx.tempo_synced_factor =
-        cx.free_running_factor * compute_tempo_synced_factor(RECIPROCAL_60_SECONDS, cx.tempo);
+    #[no_mangle]
+    pub extern "C" fn set_sample_rate(&mut self, value: f32) {
+        self.sample_rate_recip = 1. / value;
+        self.free_running_factor = compute_free_running_factor(self.rate, self.sample_rate_recip);
+        self.tempo_synced_factor = self.free_running_factor
+            * compute_tempo_synced_factor(RECIPROCAL_60_SECONDS, self.tempo);
+    }
+
+    #[no_mangle]
+    pub extern "C" fn set_rate(&mut self, value: f32) {
+        self.rate = value;
+    }
+
+    #[no_mangle]
+    pub extern "C" fn set_note_len(&mut self, value: f32) {
+        self.note_len = value;
+        let rate = note_length_to_rate(value);
+        self.set_rate(rate);
+    }
+
+    #[no_mangle]
+    pub extern "C" fn advance(&self, value: &mut f32, num_samples: i32) -> bool {
+        match self.mode {
+            SyncMode::FreeRunning => {
+                update_free_running(value, num_samples, self.free_running_factor)
+            }
+            SyncMode::TempoSync => update_tempo_sync(value, num_samples, self.tempo_synced_factor),
+            SyncMode::ProjectSync => {
+                let old_phase = *value;
+                *value = update_project_sync(self.project_time, self.rate);
+                return *value < old_phase;
+            }
+        };
+
+        return check_overflow(value, PHASE_MAX);
+    }
 }
 
 #[cfg(test)]
@@ -127,31 +131,32 @@ mod tests {
     use super::*;
     #[test]
     fn test() {
-        let phase = create();
+        let phase = Context::create();
         assert_eq!(phase.tempo, 120.);
         assert_eq!(phase.rate, 0.1);
     }
 
     #[test]
     fn test_advance() {
-        let phase = create();
+        let phase = Context::create();
         let mut value = 0.1;
-        let did_overflow = advance(phase, &mut value, 1);
+        let did_overflow = phase.advance(&mut value, 1);
         assert_eq!(did_overflow, true);
     }
 
     #[test]
     fn test_project_synced_overflow() {
         let mut phase_value = 0.;
-        let mut cx = create();
-        set_sync_mode(&mut cx, SyncMode::ProjectSync);
-        set_note_len(&mut cx, 1.0);
-        set_project_time(&mut cx, 3.9);
-        let mut overflow = advance(cx, &mut phase_value, 1);
+        let mut cx = Context::create();
+        cx.set_sync_mode(SyncMode::ProjectSync);
+        cx.set_note_len(1.0);
+        //set_project_time(&mut cx, 3.9);
+        cx.set_project_time(3.9);
+        let mut overflow = cx.advance(&mut phase_value, 1);
         assert_eq!(overflow, false);
 
-        set_project_time(&mut cx, 4.0);
-        overflow = advance(cx, &mut phase_value, 1);
+        cx.set_project_time(4.0);
+        overflow = cx.advance(&mut phase_value, 1);
         assert_eq!(overflow, true);
     }
 }
